15a48f7ba753d945b89d2e54bdf9f23e9f925d52
Jerry Zhao <u7490271@anu.edu.au>
2023-04-28 12:59:01 +1000

    implemented a login method, not yet completed. Also improved on login page

--- EducationApplication/app/src/main/java/com/example/educationapplication/MainActivity.java
+++ EducationApplication/app/src/main/java/com/example/educationapplication/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.educationapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,4 +22,18 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.login);
     }
+
+    public void login(View view) {
+        EditText email = findViewById(R.id.editTextTextEmailAddress);
+        EditText password = findViewById(R.id.editTextTextPassword);
+        // if login details are valid
+        if (1 == 1) {
+            //Add some other code to login to user specific page
+            setContentView(R.layout.activity_main);
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "No URL Entered", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
