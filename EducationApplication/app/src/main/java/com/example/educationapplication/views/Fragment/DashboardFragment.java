454147eaaede76157fe7cfc1c73034598cfd5efd
Jerry Zhao <u7490271@anu.edu.au>
2023-05-02 18:16:16 +1000

    commit before branch

--- EducationApplication/app/src/main/java/com/example/educationapplication/views/Fragment/DashboardFragment.java
+++ EducationApplication/app/src/main/java/com/example/educationapplication/views/Fragment/DashboardFragment.java
@@ -1,27 +1,12 @@
 package com.example.educationapplication.views.Fragment;
-
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-
-import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.example.educationapplication.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import org.w3c.dom.Text;
-
-import java.util.Objects;
 
 public class DashboardFragment extends Fragment implements View.OnClickListener{
 
