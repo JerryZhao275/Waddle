892ae2c71640ac33501ed6c7a83449fab4c83252
Ryan <u7124454@anu.edu.au>
2023-05-15 16:52:15 +1000

    Editing Expandable Buttons

--- EducationApplication/app/src/main/java/com/example/educationapplication/views/Fragment/QuizFragment.java
+++ EducationApplication/app/src/main/java/com/example/educationapplication/views/Fragment/QuizFragment.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.educationapplication.R;
 
-public class ForumFragment extends AppCompatActivity {
+public class QuizFragment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forum);
+        setContentView(R.layout.activity_quiz);
     }
 }
