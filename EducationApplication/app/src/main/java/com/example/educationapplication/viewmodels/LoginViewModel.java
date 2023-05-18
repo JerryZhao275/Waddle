7708ded892deed25910a322a5f32523ad23cc594
Jerry Zhao <u7490271@anu.edu.au>
2023-05-18 16:51:08 +1000

    rest of viewmodel classes commented.

--- EducationApplication/app/src/main/java/com/example/educationapplication/viewmodels/LoginViewModel.java
+++ EducationApplication/app/src/main/java/com/example/educationapplication/viewmodels/LoginViewModel.java
@@ -1,19 +1,7 @@
 package com.example.educationapplication.viewmodels;
-
-import static androidx.databinding.DataBindingUtil.setContentView;
-
-import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
-import android.widget.EditText;
-import android.widget.Toast;
-
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
-
 import com.example.educationapplication.BR;
-import com.example.educationapplication.R;
-import com.example.educationapplication.integration.database.FirebaseWaddleDatabaseServiceClient;
 import com.example.educationapplication.integration.database.WaddleDatabaseServiceClient;
 import com.example.educationapplication.integration.database.WaddleDatabaseServiceClientFactory;
 import com.example.educationapplication.integration.database.config.ConfigurationManager;
@@ -21,12 +9,8 @@ import com.example.educationapplication.integration.database.config.WaddleDataba
 import com.example.educationapplication.model.LoginModel;
 import com.example.educationapplication.util.CommonRegexUtil;
 import com.example.educationapplication.util.StringUtils;
-import com.google.firebase.auth.FirebaseAuth;
-
 import java.util.regex.Pattern;
-
 import dataObjects.CustomOnCompleteListener;
-import dataObjects.UserDto;
 
 public class LoginViewModel extends BaseObservable {
     private final WaddleDatabaseConfiguration config;
@@ -49,70 +33,123 @@ public class LoginViewModel extends BaseObservable {
         return databaseServiceClient;
     }
 
+    /**
+     * Get the email address.
+     *
+     * @return The email address.
+     */
     @Bindable
     public String getEmail() {
         return login.getEmail();
     }
 
+    /**
+     * Set the email address.
+     *
+     * @param email The email address to set.
+     */
     @Bindable
     public void setEmail(String email) {
         login.setEmail(email);
         notifyPropertyChanged(BR.email);
     }
 
+    /**
+     * Get the password.
+     *
+     * @return The password.
+     */
     @Bindable
     public String getPassword() {
         return login.getPassword();
     }
 
+    /**
+     * Set the password.
+     *
+     * @param password The password to set.
+     */
     @Bindable
     public void setPassword(String password) {
         login.setPassword(password);
         notifyPropertyChanged(BR.password);
     }
 
+    /**
+     * Get the error message.
+     *
+     * @return The error message.
+     */
     @Bindable
     public String getErrorMessage() {
         return login.getErrorMessage();
     }
 
+    /**
+     * Set the error message.
+     *
+     * @param errorMessage The error message to set.
+     */
     @Bindable
     public void setErrorMessage(String errorMessage) {
         login.setErrorMessage(errorMessage);
         notifyPropertyChanged(BR.errorMessage);
     }
 
+    /**
+     * Check if the user is authorized.
+     *
+     * @return True if the user is authorized, false otherwise.
+     */
     public boolean isAuthorised() {
         return authorised;
     }
 
+    /**
+     * Set the authorization status.
+     *
+     * @param authorised The authorization status to set.
+     */
     public void setAuthorised(boolean authorised) {
         this.authorised = authorised;
     }
 
+    /**
+     * Performs the login operation.
+     *
+     * @param listener The listener to be called when the login operation is complete.
+     */
     public void login(CustomOnCompleteListener listener) {
         boolean questionsAnswered = StringUtils.isNotEmpty(login.getEmail()) && StringUtils.isNotEmpty(login.getPassword());
         boolean isEmailValid = Pattern.matches(CommonRegexUtil.EMAIL, login.getEmail());
+
+        // Check if all fields are filled
         if (!questionsAnswered) {
             setErrorMessage(EMPTY_FIELD);
             setAuthorised(false);
             return;
         }
+
+        // Check if the email is valid
         if (!isEmailValid) {
             setErrorMessage(LOGIN_FAILED);
             setAuthorised(false);
             return;
         }
+
+        // Perform the sign-in operation
         getDatabaseServiceClient().signIn(login.getEmail(), login.getPassword(), () -> {
             if (getDatabaseServiceClient().getCurrentUser() == null) {
+                // User authentication failed
                 setErrorMessage(INVALID_USER);
                 setAuthorised(false);
                 return;
             }
+
+            // User authentication succeeded
             setAuthorised(true);
             setErrorMessage(null);
             listener.onComplete();
         });
     }
-
 }
